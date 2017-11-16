9aa63bfac81847955b5b7c4e2ab50f0b77f0879c
Mark Anthony Gibbins <xiy@protonmail.ch>
2017-11-16 17:01:20 +0000

    Better gzip+b64 testing

--- gzip_test.go
+++ gzip_test.go
@@ -6,16 +6,17 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-var b64enc = "H4sIAAAAAAAE/wAFAPr/aGVsbG8BAAD//4amEDYFAAAA"
+var b64enc = "H4sIAAAAAAAE/wAPAPD/PGgxPkhlbGxvITwvaDE+AQAA//9WO9LMDwAAAA=="
+var plainString = `<h1>Hello!</h1>`
 
 func TestCompression(t *testing.T) {
 	Convey("it should compress a string correctly", t, func() {
-		So(Compress("hello"), ShouldEqual, b64enc)
+		So(Compress(plainString), ShouldEqual, b64enc)
 	})
 }
 
 func TestDecompression(t *testing.T) {
 	Convey("it should decompress a string", t, func() {
-		So(Decompress(b64enc), ShouldEqual, "hello")
+		So(Decompress(b64enc), ShouldEqual, plainString)
 	})
 }
